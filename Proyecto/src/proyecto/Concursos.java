431e42976ff5ab1dda00b2b1251b3593b5866275
nathalyCrespo <npcrespo@espol.edu.ec>
2021-11-12 18:05:21 -0500

    creacion de las clases

---
+++ Proyecto/src/proyecto/Concursos.java
@@ -0,0 +1,14 @@
+
+package proyecto;
+
+
+public class Concursos {
+    public String nombre;
+    public String fechaEvento;
+    public String horaEvento;
+    public String fechaInscripcion;
+    public String fechaFinIncripcion;
+    public String lugar;
+    public String CodConcurso;
+    
+}
