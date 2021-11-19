fcb77100ad72dc633e32e8025a31298f7d1d5368
gjareval <gjareval@espol.edu.ec>
2021-11-18 23:15:02 -0500

    Atributos Concurso

--- Proyecto/src/modelo/Concurso.java
+++ Proyecto/src/modelo/Concurso.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 public class Concurso {
     public String nombre;
     public LocalDate fechaEvento;
-    public LocalDateTime horaEvento;
+    public LocalTime horaEvento;
     public LocalDate fechaInscripcion;
-    public LocalDate fechaFinIncripcion;
+    public LocalDate fechaFinInscripcion;
     public Ciudad ciudad;
     public String lugar;
     public String CodConcurso;
@@ -24,5 +24,21 @@ public class Concurso {
     public String crearCodConc(String fechaEvento, String ciudad, String lugar){
         return 
     
+    }
+
+    public Concurso(String n, LocalDate fe, LocalTime he, LocalDate fi, LocalDate ff, Ciudad ci, String lu, String cod, Premio pr,ArrayList<Auspiciante> arr, TiposAnimal ta){
+        nombre= n;
+        fechaEvento=fe;
+        horaEvento=he;
+        fechaInscripcion=fi;
+        fechaFinInscripcion=ff;
+        ciudad=ci;
+        lugar=lu;
+        CodConcurso=cod;
+        premio=pr;
+        auspiciantes=arr;
+        dirigidoA=ta;
+
+        
     }
 }
