1800c733c0b187f90ccfa0b791cef7275ab6d4b7
santi0ne <johsrodr@espol.edu.ec>
2021-11-18 22:41:53 -0500

    clase puesto con atributos

--- Proyecto/src/modelo/Premio.java
+++ Proyecto/src/modelo/Premio.java
@@ -1,5 +1,11 @@
 package modelo;
 
 public class Premio {
+    private Puesto puesto;
+    private String descripcion;
+    private String auspiciante;
     
+    public void crearPremio(){
+        
+    }
 }
