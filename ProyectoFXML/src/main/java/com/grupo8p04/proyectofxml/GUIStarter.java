d59aa53bf271a24bb6640f2eb57a59d4c23ebb98
gjareval <gjareval@espol.edu.ec>
2021-12-23 10:45:38 -0500

    Modificación archivo jar

---
+++ ProyectoFXML/src/main/java/com/grupo8p04/proyectofxml/GUIStarter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo8p04.proyectofxml;
+
+/**
+ *
+ * @author Guillermo José
+ */
+public class GUIStarter {
+    
+    public static void main(String args[]){
+        App.main(args);
+    }
+    
+}
