124f55d9a35d9f93edacbe890f1cbad3aaa68152
santi0ne <johsrodr@espol.edu.ec>
2021-12-23 22:01:48 -0500

    adminMascota hecho

---
+++ ProyectoFXML/src/main/java/com/grupo8p04/proyectofxml/CrearMascotaController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo8p04.proyectofxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author hp
+ */
+public class CrearMascotaController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
